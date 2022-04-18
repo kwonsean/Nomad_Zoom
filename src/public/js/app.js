6f9dcdc4c509828b61a521e72a57170bf1a6e6b6
kwonsean <gownn12@naver.com>
2022-04-18 23:37:01 +0900

    feat: Connect with JSON
    - Add nickname, newMsg type

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,4 +1,12 @@
+const $msgList = document.querySelector("ul");
+const $nickNameForm = document.querySelector("#nickName");
+const $msgForm = document.querySelector("#msg");
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMsg(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
   console.log("Connected to server!");
@@ -6,13 +14,33 @@ socket.addEventListener("open", () => {
 
 // 서버에서 보낸 메시지 확인
 socket.addEventListener("message", (msg) => {
-  console.log("msg : " + msg.data + " from server");
+  const li = document.createElement("li");
+  li.textContent = msg.data;
+  $msgList.appendChild(li);
+  console.log("New msg : " + msg.data);
 });
 
 socket.addEventListener("close", () => {
   console.log("Disconnected from Server...");
 });
 
-setTimeout(() => {
-  socket.send("Hello Sever! It's browser~");
-}, 5000);
+// setTimeout(() => {
+//   socket.send("Hello Sever! It's browser~");
+// }, 5000);
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const input = $msgForm.querySelector("input");
+  socket.send(makeMsg("newMsg", input.value));
+  input.value = "";
+};
+
+const handleNickSubmit = (e) => {
+  e.preventDefault();
+  const input = $nickNameForm.querySelector("input");
+  socket.send(makeMsg("nickName", input.value));
+  input.value = "";
+};
+
+$msgForm.addEventListener("submit", handleSubmit);
+$nickNameForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -18,11 +18,31 @@ const server = http.createServer(app);
 const webSocketServer = new WebSocket.Server({ server });
 // 3000포트에서 http와 ws 모두 사용 가능
 
+// 연결이 될때마다 소켓을 담는 배열
+// 이 소켓에는 각 브라우져의 소캣이 담기게되고 이 배열을 이용하여 다른 브라우저에도 메시지 전달 가능
+const sockets = [];
+
+// 아래 코드는 브라우저(클라이언트)에 연결될때 마다 실행
 webSocketServer.on("connection", (socket) => {
+  sockets.push(socket);
+  socket.nickName = "NOBODY";
   console.log("Connected to Browser!");
   socket.on("close", () => console.log("Disconnected to Browser!"));
-  socket.on("message", (msg) => console.log(msg.toString("utf8")));
-  socket.send("Hello client!");
+  socket.on("message", (msg) => {
+    const msgDecode = msg.toString("utf8");
+    const { type, payload: data } = JSON.parse(msgDecode);
+    switch (type) {
+      case "newMsg":
+        sockets.filter((item) => item !== socket).forEach((aSocket) => aSocket.send(`${socket.nickName} : ${data}`));
+        break;
+      case "nickName":
+        console.log(data);
+        socket.nickName = data;
+        break;
+      default:
+        break;
+    }
+  });
 });
 
 server.listen(3000, handleListen);
